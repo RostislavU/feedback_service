caad86dac83af61738fcb6f37ff7e3d4aeae8d2e
RostislavU <rostislav.udalov@yandex.ru>
2021-04-11 09:02:00 +0300

    Добавление для функционала Questions - Сущность для изменения вопроса - Контроллер - Сервис
    Signed-off-by: RostislavU <rostislav.udalov@yandex.ru>

---
+++ src/main/java/ru/experts/feedback/controllers/QuestionController.java
@@ -0,0 +1,44 @@
+package ru.experts.feedback.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.experts.feedback.domain.Question;
+import ru.experts.feedback.dto.EditQuestionRequest;
+import ru.experts.feedback.services.question.QuestionService;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@Slf4j
+@RequestMapping("/questions")
+public class QuestionController {
+
+    public QuestionController() {}
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping()
+    public List<Question> getAll(){
+        return questionService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Question getById(@PathVariable("id") UUID id){
+        return questionService.getById(id);
+    }
+
+    @PostMapping("/create")
+    public Question create(@RequestBody EditQuestionRequest request){
+        return questionService.create(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") UUID id){
+        questionService.deleteById(id);
+    }
+
+
+}
